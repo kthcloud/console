d7a5d9fa7e8fc30d524a9540094733389635db36
pierrelefevre <hello@pierrelf.com>
2023-11-10 15:22:05 +0100

    Add footer for genAI content, closes #97

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,12 +1,15 @@
 import Hero from "./components/hero/Hero";
 import Intro from "./components/intro/Intro";
 import Page from "../../components/Page";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useKeycloak } from "@react-keycloak/web";
 import LoadingPage from "src/components/LoadingPage";
+import Iconify from "src/components/Iconify";
+import { useTranslation } from "react-i18next";
 
 export function Landing() {
   const { keycloak, initialized } = useKeycloak();
+  const {t} = useTranslation();
 
   if (!initialized) {
     return (
@@ -23,6 +26,7 @@ export function Landing() {
         <Box mt={5}></Box>
         <Hero />
         <Intro />
+        <Typography variant="body2" align="center" mt={5} sx={{ display: { sm: "none" }}}><Iconify icon="twemoji:sparkles" /> {t("this-content-is-ai-generated")}</Typography>
       </Page>
     );
   }
