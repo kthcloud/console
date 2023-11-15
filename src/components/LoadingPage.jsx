b2520a283b77823d0d38cda69f5eac3e99dfd0cd
Pierre Le Fevre <hello@pierrelf.com>
2023-11-15 15:57:14 +0100

    Add retry indicator, closes #100

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,7 +1,22 @@
 import { CircularProgress, Grid } from "@mui/material";
 import Page from "./Page";
+import useResource from "src/hooks/useResource";
+import useInterval from "src/hooks/useInterval";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const LoadingPage = () => {
+  const { nextLoad, connectionError } = useResource();
+  const [retryIn, setRetryIn] = useState(0);
+  const { t, i18n } = useTranslation();
+
+  useInterval(() => {
+    let now = new Date();
+    let diff = new Date(nextLoad) - now;
+    let seconds = Math.floor(diff / 1000);
+    setRetryIn(seconds);
+  }, 500);
+
   const loadingMessages = [
     "Assembling cloud formations...",
     "Uploading to cloud nine...",
@@ -20,6 +35,8 @@ const LoadingPage = () => {
   ];
 
   const getLoadingMessage = () => {
+    if (connectionError) return t("connectionError");
+    if(i18n.language !== "en") return t("loading");
     // Get useragent and current minute to ensure loading message is consistent through refreshes
     const agent = window.navigator.userAgent;
     const minutes = new Date().getMinutes().toString();
@@ -49,6 +66,11 @@ const LoadingPage = () => {
           <Grid item xs={3} m={1}>
             <div>{getLoadingMessage()}</div>
           </Grid>
+          {connectionError && retryIn > 0 && (
+            <Grid item xs={3} m={1}>
+              <div>{`${t("retrying-in")} ${retryIn} ${t("seconds")}`}</div>
+            </Grid>
+          )}
         </Grid>
       </Page>
     </>
