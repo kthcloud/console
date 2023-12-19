baf5b8140bf7d342dc1b37ddc8250b77120660e8
Pierre Le Fevre <hello@pierrelf.com>
2023-12-19 20:06:52 +0100

    Fix format

--- src/contexts/ThemeModeContext.jsx
+++ src/contexts/ThemeModeContext.jsx
@@ -2,28 +2,27 @@ import { useMediaQuery } from "@mui/material";
 import { createContext, useState } from "react";
 
 const initialState = {
-    mode: "light",
+  mode: "light",
+};
+
+export const ThemeModeContext = createContext({
+  ...initialState,
+});
+
+export const ThemeModeContextProvider = ({ children }) => {
+  const initial = useMediaQuery("(prefers-color-scheme: dark)")
+    ? "dark"
+    : "light";
+
+  const [mode, setMode] = useState(initial);
+
+  const toggleMode = () => {
+    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
   };
-  
-  export const ThemeModeContext = createContext({
-    ...initialState,
-  });
-  
-  export const ThemeModeContextProvider = ({ children }) => {
-    const initial = useMediaQuery("(prefers-color-scheme: dark)")
-      ? "dark"
-      : "light";
-  
-    const [mode, setMode] = useState(initial);
-  
-    const toggleMode = () => {
-      setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-    };
-  
-    return (
-      <ThemeModeContext.Provider value={{ mode, setMode, toggleMode }}>
-        {children}
-      </ThemeModeContext.Provider>
-    );
-  };
-  
+
+  return (
+    <ThemeModeContext.Provider value={{ mode, setMode, toggleMode }}>
+      {children}
+    </ThemeModeContext.Provider>
+  );
+};
