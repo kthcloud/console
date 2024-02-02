cb55e81cf6637151996cb1f6bc5433ccfda66643
Pierre Le Fevre <hello@pierrelf.com>
2024-02-02 16:28:56 +0100

    Add domain logo switcher, closes #184

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -14,10 +14,20 @@ export default function Logo({ disabledLink = false, sx }) {
   const { initialized, keycloak } = useKeycloak();
   const { mode } = useContext(ThemeModeContext);
 
+  // get window.location.hostname
+
   const logo = (
     <Box sx={{ width: 140, height: "auto", ...sx }}>
       <img
-        src={"/static/logo_" + mode + ".svg"}
+        src={
+          "/static/logo_" +
+          (window.location.hostname.includes("cloud.cbh.kth.se")
+            ? "cbh"
+            : "kth") +
+          "_" +
+          mode +
+          ".svg"
+        }
         className="App-logo"
         alt="logo"
       />
