02a50a6c4811b9b7828a8d3b11cae82964b50be5
Pierre Le Fevre <hello@pierrelf.com>
2024-02-09 11:42:09 +0100

    Add million.js, closes #186

--- index.jsx
+++ index.jsx
@@ -3,10 +3,11 @@ import "simplebar/dist/simplebar.css";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { initI18n } from "./i18next";
+import { initI18n } from "./src/i18next.js";
 //
-import App from "./App.jsx";
-import "./theme/customStyle.css";
+import App from "./src/App.jsx";
+import "./src/theme/customStyle.css";
+
 // ----------------------------------------------------------------------
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
