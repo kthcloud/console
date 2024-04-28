a6b076d3d204cbe294a824adaece7527f551270a
Pierre Le Fevre <hello@pierrelf.com>
2024-04-28 18:13:47 +0200

    Change default alert type to outlined #231

--- src/components/AlertList.tsx
+++ src/components/AlertList.tsx
@@ -43,7 +43,7 @@ export const AlertList = () => {
         severity={alert.severity || "info"}
         sx={{ width: "100%", my: 5 }}
         key={"alert-" + index}
-        variant={alert.variant || "filled"}
+        variant={alert.variant || "outlined"}
         action={
           alert.link ? (
             <IconButton color="inherit" size="small" href={alert.link}>
